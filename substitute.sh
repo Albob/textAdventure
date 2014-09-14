f2c26c52ad3a2d213fcad65a2f9a1a3cd7eff9fd
Alexis Bauchu <alexis@prettysimplegames.com>
2014-09-14 18:55:22 +0200

    [substitute] Add documentation

--- substitute.sh
+++ substitute.sh
@@ -1,3 +1,9 @@
+#
+# substitute, by Alexis Bauchu, 2014
+# This script allows substitution of a string is several files.
+# Files where the substitution has been made are backed up in a .bak file.
+#
+
 search=$1
 replace=$2
 ack -li $search | xargs sed -i.bak "s/$search/$replace/g"

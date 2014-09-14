e5f9149565b1f96917da0b93f958e734214a32b4
Alexis Bauchu <alexis@prettysimplegames.com>
2014-09-14 20:07:50 +0200

    [main] Add (very) basic reading of characters form stdin

--- textAdventure.c
+++ textAdventure.c
@@ -6,16 +6,37 @@
 */
 
 #include <albob/string.h>
+#include <ctype.h>
 #include <stdio.h>
 
 void debug(StringRef ref)
 {
-    printf("Ref: %u, Content: \"%s\"\n", ref, strCStr(ref));
+    printf("<DEBUG>Ref: %u, Content: \"%s\"\n", ref, strCStr(ref));
 }
 
 int main(int arg_number, char * arguments[])
 {
+    puts("Welcome, to ADVENTURE GAME!!");
+    int must_exit = 0;
+
+    do
+    {
+        int c = 10;
+        do
+        {
+            c = getchar();
+
+            if (c == EOF)
+            {
+                must_exit = 1;
+                break;
+            }
+            printf("You wrote '%c'\n", c);
+        } while (c != '\n');
+    } while (must_exit == 0);
+
     // exit
+    puts("See you next time!!");
     return 0;
 }
 

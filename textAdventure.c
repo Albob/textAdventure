0c9629ed7a4894d6766621925c8cb2e561cc2648
Alexis Bauchu <alexis@prettysimplegames.com>
2014-09-14 22:37:47 +0200

    [WIP] On the way to read one char-at-a-time from stdin

--- textAdventure.c
+++ textAdventure.c
@@ -8,10 +8,26 @@
 #include <albob/string.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <termios.h>
 
 void debug(StringRef ref)
 {
     printf("<DEBUG>Ref: %u, Content: \"%s\"\n", ref, strCStr(ref));
+}
+
+
+void ttyCanonOff(void)
+{
+    tcgetattr(fileno(stdin), &t); //get the current terminal I/O structure
+    t.c_lflag &= ~ICANON; //Manipulate the flag bits to do what you want it to do
+    tcsetattr(fileno(stdin), TSCANOW, &t); //Apply the new settings
+}
+
+void ttyCanonOn(void)
+{
+    tcgetattr(STDIN_FILENO, &t); //get the current terminal I/O structure
+    t.c_lflag |= ICANON; //Manipulate the flag bits to do what you want it to do
+    tcsetattr(STDIN_FILENO, TSCANOW, &t); //Apply the new settings
 }
 
 int main(int arg_number, char * arguments[])
@@ -31,7 +47,12 @@ int main(int arg_number, char * arguments[])
                 must_exit = 1;
                 break;
             }
-            printf("You wrote '%c'\n", c);
+            else if (c == '\n') {
+                putchar('\n');
+            }
+            else {
+                putchar('x');
+            }
         } while (c != '\n');
     } while (must_exit == 0);
 
